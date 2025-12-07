46b7dcf5f2bd55146099094c33744ef4e75fbb55
Mac <mac@Macs-MacBook-Pro.local>
2025-12-07 13:58:26 +0700

    baitap39

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import Redux from './pages/redux/index'
 import Deposit from './pages/deposit/index'
 import './App.css'
 import TodoApp from './pages/TodoApp'
-
+import Navigator from './components/ui/Navigator/Navigator'
 function App() {
-    
+
 
   return (
     <Router basename={import.meta.env.PROD ? "/f8-fullstack-38" : "/"}>
+      <Navigator />
       <Routes>
         <Route path="/deposit" element={<Deposit />}></Route>
         <Route path="/redux" element={<Redux />}></Route>
-        <Route path="/todoapp" element = {<TodoApp/>}></Route>
+        <Route path="/todoapp" element={<TodoApp />}></Route>
       </Routes>
     </Router>
   )
---
+++ src/components/ui/Navigator/Navigator.jsx
@@ -0,0 +1,13 @@
+import { NavLink , Link } from "react-router";
+
+function Navigator(){
+              <nav>
+                    <ul>
+                        <li> <Link to="/deposit">deposit</Link></li>
+                        <li> <Link to="/redux">redux</Link></li>
+                        <li> <Link to="/todoapp">TodoApp</Link></li>
+                    </ul>
+                </nav>
+}
+
+export default Navigator;
